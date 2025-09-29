8796e6a968929c5c8f0cbc4fd9344fcaba8af726
Julien V <vier.julien@googlemail.com>
2025-09-29 21:51:44 +0100

    update sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,6 @@ import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://couleuraleatoire.netlify.app',
+  site: 'https://generateurdecouleur.com',
   integrations: [tailwind(), sitemap()]
 });
